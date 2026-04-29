706e7174537f731113ffa97531d8ef5fb868520a
Samuel Luciano <108229765+SamuelL10Br@users.noreply.github.com>
2026-04-29 18:30:54 +0100

    Delete Projeto Planilha Completo directory

--- Projeto Planilha Completo/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("api", {
-  obterClientes: () => ipcRenderer.invoke("clientes:obter"),
-  salvarClientes: (clientes) => ipcRenderer.invoke("clientes:salvar", clientes),
-  recarregarClientes: () => ipcRenderer.invoke("clientes:recarregar"),
-  storageGet: (chave) => ipcRenderer.invoke("storage:get", chave),
-  storageSet: (chave, valor) => ipcRenderer.invoke("storage:set", chave, valor)
-});
